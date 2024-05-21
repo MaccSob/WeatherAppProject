e49510e61901719454206d67f7eb0a1a932efda2
MaccSob <71sobmaciej@gmail.com>
2024-05-21 21:01:34 +0200

    Api up and working, working on CSS now

--- WeatherApp/src/index.js
+++ WeatherApp/src/index.js
@@ -1,16 +1,32 @@
+var descrip = document.querySelector('#description')
+var temp = document.querySelector('#temp')
+var wind = document.querySelector('#wind')
+var cityname = document.querySelector('#cityname')
+var pressure = document.querySelector('#pressure');
+var humidity = document.querySelector('#humidity');
 
+const weatherD = document.querySelector('#weather');
 const button = document.querySelector('button');
-const city = document.querySelector('#weather').value;
-
-console.log(city);
+const apiKey = '24b17c0eac6ec17765a51e4e7f68330a'
 
 
+button.addEventListener('click', function(){
+  fetch('https://api.openweathermap.org/data/2.5/weather?q='+weatherD.value+'&appid='+apiKey+'&lang=pl')
+  .then(res => res.json())
+  .then(data => {
+      var nameval = data['name']
+      var temperature = data['main']['temp']
+      var descrip = data['weather']['0']['description']
+      var wndspd = data['wind']['speed']
+      var press = data['main']['pressure']
+      var hum = data['main']['humidity']
 
 
-async function getWeather() {
-
-const response = await fetch(`http://api.weatherapi.com/v1/search.json?key=3c7acc06e2dc4fcfb39214313241505&q=${city}`)
-const data = await response.json();
-console.log(data);
-}
-button.addEventListener("click", getWeather());
+    temp.innerHTML = `Temperatura: ${(Math.round(temperature - 273.15))}`+ 'C°';
+      cityname.innerHTML=`Nazwa: ${nameval}`
+      description.innerHTML = `Zachmurzenie: ${descrip}`
+      wind.innerHTML = `Wiatr: ${wndspd} km/h`
+      pressure.innerHTML = `Ciśnienie: ${press}` + 'hpa'
+    humidity.innerHTML = `Wilgotność: ${hum}` + '%'
+  })
+})
